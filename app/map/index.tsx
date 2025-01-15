af640cd21173881c71efa8b41000160477d35cdb
Axely7 <axely7@hotmail.com>
2025-01-15 12:03:57 -0600

    prebuild ios and android

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 import React from "react";
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
--- core/actions/permissions/location.ts
+++ core/actions/permissions/location.ts
@@ -1,12 +1,14 @@
 import { PermissionStatus } from '@/infrastructure/interfaces/location'
 import * as Location from 'expo-location'
-import { Alert } from 'react-native'
+import { Alert, Linking } from 'react-native'
 
 
 export const requestLocationPermission = async():Promise<PermissionStatus> => {
     const {status} = await Location.requestForegroundPermissionsAsync()
     if(status !== 'granted'){
-        manualPermissionRequest()
+        if(status === "denied"){
+            manualPermissionRequest()
+        }
         return PermissionStatus.DENIED
     }
 
@@ -32,6 +34,19 @@ export const checkLocationPermission = async() => {
 
 const manualPermissionRequest = async() => {
     Alert.alert(
-        'Permiso de ubicación necesario'
+        'Permiso de ubicación necesario',
+        "Para continuar debe de habilitar el permiso de location en los ajustes del teléfono",
+        [
+            {
+                text: 'Abrir ajustes',
+                onPress: () => {
+                    Linking.openSettings()
+                }
+            },
+            {
+                text: "Cancelar",
+                style: "destructive"
+            }
+        ]
     )
 }
